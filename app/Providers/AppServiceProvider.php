f08dff39c3b2243d10bcf1e013dc81e08ed11821
Wilson-Ochieng <wilsonochieng718@gmail.com>
2026-09-25 21:36:33 +0300

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,16 +9,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+  public function boot(): void
+{
+    if (app()->environment('production')) {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+    if ($root = config('app.url')) {
+        \Illuminate\Support\Facades\URL::forceRootUrl($root);
     }
+}
 }
